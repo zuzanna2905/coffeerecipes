5c73b3b2814c44f950f6702132cc021f03cacfe4
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-06-09 14:52:56 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Roaster.create(name: 'Audun Coffee', country: 'Poland')
+# Roaster.create(name: 'Cloud Picker', country: 'Irleand')
+# Roaster.create(name: 'Five Elephant', country: 'Germany')
+# Roaster.create(name: 'Hard Beans', country: 'Poland')
+# Roaster.create(name: 'Hayb', country: 'Poland')
+# Roaster.create(name: 'Java Coffee', country: 'Poland')
+# Roaster.create(name: 'Populus Coffee', country: 'Germany')
+# Roaster.create(name: 'Simplo', country: 'Poland')
+# Roaster.create(name: 'Johan & Nystrom', country: 'Sweden')
+# Roaster.create(name: 'Fjord', country: 'Germany')
