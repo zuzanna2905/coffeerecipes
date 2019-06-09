5b6be80cc0755ddbe730c345318c34f666c8df2f
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-06-09 14:36:42 +0200

    add roasters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_123253) do
+ActiveRecord::Schema.define(version: 2019_06_09_123556) do
 
   create_table "beans", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2019_06_09_123253) do
     t.string "grinder"
     t.integer "clicks"
     t.integer "user_id"
+  end
+
+  create_table "roasters", force: :cascade do |t|
+    t.string "name"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
