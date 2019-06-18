10432d385b981790540c149903229a3c72ed9649
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-06-18 12:56:04 +0200

    add users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Jon', email: 'jon@gmail.com', password: 'jon123')
+User.create(name: 'Arya', email: 'arya@gmail.com', password: 'arya123')
+User.create(name: 'Sansa', email: 'sansa@gmail.com', password: 'sansa123')
+User.create(name: 'Bran', email: 'bran@gmail.com', password: 'bran123')
+User.create(name: 'Rob', email: 'rob@gmail.com', password: 'rob123')
+User.create(name: 'Rickon', email: 'rickon@gmail.com', password: 'rickon123')
