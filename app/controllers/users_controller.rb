ddda90569ba79bab0dbebe64e9b5a2ba56f09c91
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-06-18 20:13:46 +0200

    styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     before_action :require_same_user, only: [:edit, :update]
 
     def index
-        @users = User.all
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def new
