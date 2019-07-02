c50714c5267659ff8fc4c00eb767c3a236d22f0e
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-07-02 16:22:24 +0200

    bean model tests

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -1,4 +1,9 @@
 class Bean < ApplicationRecord
     belongs_to :roaster, foreign_key: "roaster_id"
     has_many :recipes
+    validates :name, presence: true, length: { minimum: 3, maximum: 50}
+    validates :roaster_id, presence: true
+    validates :method, presence: true, length: { minimum: 3, maximum: 20}
+    validates :country, presence: true, length: { minimum: 2, maximum: 20}
+    validates :roast, presence: true, length: { minimum: 3, maximum: 20}
 end
---
+++ spec/models/bean_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Bean, type: :model do
+  subject { described_class.new(name: 'Peru El Diamante', method: 'Washed', 
+                                country: 'Peru', roast: 'light', 
+                                description: 'Varietal: Caturra', roaster_id: 1)}
+
+  describe "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:roaster_id) }
+    it { should validate_presence_of(:method) }
+    it { should validate_presence_of(:country) }
+    it { should validate_presence_of(:roast) }
+  end
+
+  describe "Associations" do
+  it { should have_many(:recipes) }
+  it { should belong_to(:roaster) }
+  end
+end
