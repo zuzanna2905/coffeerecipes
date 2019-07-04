b678a67517bbadf02f3316f63c0d492fe526af0d
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-07-04 22:55:56 +0200

    roaster model tests

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -1,4 +1,7 @@
 class Roaster < ApplicationRecord
     has_many :beans, dependent: :destroy
     has_many :recipes
+
+    validates :name, presence: true, length: { minimum: 3, maximum: 50}
+    validates :country, presence: true, length: { minimum: 2, maximum: 20}
 end
---
+++ spec/models/roaster_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Roaster, type: :model do
+  subject { described_class.new(name: 'Audun Coffee', country: 'Poland')}
+
+  describe "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:country) }
+  end
+
+  describe "Associations" do
+    it { should have_many(:recipes) }
+    it { should have_many(:beans) }
+  end
+end
