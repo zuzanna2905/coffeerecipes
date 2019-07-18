e9a5d67be793f3ee5728e83582ebf4221f12e132
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-07-18 17:12:02 +0200

    update migartion for postgresql

--- db/migrate/20190620173629_change_recipe_columns_type.rb
+++ db/migrate/20190620173629_change_recipe_columns_type.rb
@@ -1,6 +1,6 @@
 class ChangeRecipeColumnsType < ActiveRecord::Migration[5.2]
   def change
-    change_column :recipes, :roaster_id, :integer
-    change_column :recipes, :bean_id, :integer
+    change_column :recipes, :roaster_id, 'integer USING CAST(roaster_id AS integer)'
+    change_column :recipes, :bean_id, 'integer USING CAST(bean_id AS integer)'
   end
 end
