3e0d35a8b5a2ff8b75a38a537006372bd27a89c2
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-07-22 22:48:54 +0200

    update logins controller

--- spec/controllers/logins_controller_spec.rb
+++ spec/controllers/logins_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe LoginsController, type: :controller do
+    let(:user) { create :user}
     it 'show NEW page' do
         get :new
         expect(response).to render_template('logins/new')
     end
 
     it 'redirect to recipes from NEW if is logged in' do
-        @user = create_user 
-        login(@user)
+        login(user)
         get :new
         expect(response).to redirect_to('/recipes')
     end
