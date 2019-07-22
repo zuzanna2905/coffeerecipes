20a3434cf92d51944ab372aaf0240da52847b02b
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-07-22 22:51:06 +0200

    update roaster controller

--- spec/controllers/roasters_controller_spec.rb
+++ spec/controllers/roasters_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe RoastersController, type: :controller do
+    let(:roaster) { create :roaster}
     it 'render the INDEX' do
         get :index
         expect(response).to render_template('index')
     end
     
     it 'SHOW roaster' do
-        @roaster = create_roaster
-        get :show, params: { id: @roaster.id }
+        get :show, params: { id: roaster }
         expect(response).to render_template('show')
     end
 end
